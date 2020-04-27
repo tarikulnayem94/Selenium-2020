0cc286628a0548dc12b8b7be845392fdeb1555d7
Nayeem <Nayeem@Nayem>
2020-04-27 17:04:35 +0600

    selenium with java

---
+++ Jenkins/src/com/Jenkins/JenkinsDemo.java
@@ -0,0 +1,13 @@
+package com.Jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+
+	@Test
+	public void JenkinsTest() {
+		System.out.println(" Welcome to jenkins world ");
+	}
+
+}
+
